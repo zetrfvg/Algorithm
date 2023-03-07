5c9daebf8306c7694f0660bd275a29606bc259f2
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-07 23:52:52 +0800

    Create 547. 省份数量.cpp
    合并集合&统计集合数量

---
+++ 并查集/经典题/547. 省份数量.cpp
@@ -0,0 +1,34 @@
+template<typename T>
+struct DSU{
+	int n;
+	vector<T> p,siz;
+	DSU(int n):p(n+1),siz(n+1){
+		iota(p.begin(),p.end(),0);
+		for(int i=0;i<n+1;i++) siz[i]=1;
+	}
+	T findd(T x){
+		return p[x]==x?x:p[x]=findd(p[x]);
+	}
+	void unionn(T x,T y){
+		x=findd(x),y=findd(y);
+		if(x==y) return;
+		if(siz[x]>siz[y]) swap(x,y);
+		p[x]=y;
+		siz[y]+=siz[x];
+	}
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        DSU<int> dsu(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]) dsu.unionn(i,j);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++) if(dsu.p[i]==i) ans++;
+        return ans;
+    }
+};
