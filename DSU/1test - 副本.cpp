e1f6b97158e25ce3b1b4864efdd7284a33b874d4
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-07 21:27:04 +0800

    Add files via upload

---
+++ DSU/1test - 副本.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for(int i = a;i<n;i++)
+#define per(i,a,n) for(int i = n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+typedef vector<int> VI;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef double db;
+mt19937 mrand(random_device{}());
+const ll mod=1000000007;
+int rnd(int x) {return mrand()%x;}
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(int a,int b){return a*b/gcd(a,b);}
+
+void solve(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int T=1;
+	cin>>T;
+	while(T--){
+		solve();
+	}
+	return 0;
+}
