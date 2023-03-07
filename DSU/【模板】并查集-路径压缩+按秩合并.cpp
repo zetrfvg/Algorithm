c68e192a63f5255625b1a5265b428f040f35f84e
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-07 21:27:29 +0800

    Add files via upload

---
+++ DSU/【模板】并查集-路径压缩+按秩合并.cpp
@@ -0,0 +1,19 @@
+template<typename T>
+struct DSU{
+	int n;
+	vector<T> p,siz;
+	DSU(int n):p(n+1),siz(n+1){
+		iota(all(p),0);
+		for(int i=0;i<n+1;i++) siz[i]=1;
+	}
+	T findd(T x){
+		return p[x]==x?x:p[x]=findd(p[x]);
+	}
+	void unionn(T x,T y){
+		x=findd(x),y=findd(y);
+		if(x==y) return;
+		if(siz[x]>siz[y]) swap(x,y);
+		p[x]=y;
+		siz[y]+=siz[x];
+	}
+};
