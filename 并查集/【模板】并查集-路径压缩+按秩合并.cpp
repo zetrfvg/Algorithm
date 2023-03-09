652e85611e107c9e16b80a55e8bc3c57686feff3
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-09 10:37:54 +0800

    Update 【模板】并查集-路径压缩+按秩合并.cpp

--- 并查集/【模板】并查集-路径压缩+按秩合并.cpp
+++ 并查集/【模板】并查集-路径压缩+按秩合并.cpp
@@ -2,9 +2,9 @@ template<typename T>
 struct DSU{
 	int n;
 	vector<T> p,siz;
-	DSU(int n):p(n+1),siz(n+1){
+	DSU(int _n):p(n+1),siz(n+1),n(_n){
 		iota(p.begin(),p.end(),0);
-		for(int i=0;i<n+1;i++) siz[i]=1;
+		for(int i=0;i<=n;i++) siz[i]=1;
 	}
 	T findd(T x){
 		return p[x]==x?x:p[x]=findd(p[x]);
