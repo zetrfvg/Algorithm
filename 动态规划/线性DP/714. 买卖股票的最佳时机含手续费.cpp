06e4f72e589c380db6a50e91b5627ce1ea78ad05
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-15 14:22:13 +0800

    Create 714. 买卖股票的最佳时机含手续费.cpp

---
+++ 动态规划/线性DP/714. 买卖股票的最佳时机含手续费.cpp
@@ -0,0 +1,21 @@
+/*
+定义状态：
+dp[i][0]:在第i天没有股票的同时获得的最大价值
+dp[i][1]:在第i天持有股票的同时获得的最大价值
+状态转移：
+dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i]);
+dp[i][1]=max(dp[i-1][1],dp[i-1][0]-prices[i]-fee);
+*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,0));
+        dp[0][1]=-prices[0]-fee;
+        for(int i=1;i<n;i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i]);
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]-prices[i]-fee);
+        }
+        return dp[n-1][0];
+    }
+};
