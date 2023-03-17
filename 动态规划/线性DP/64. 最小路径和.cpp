63b25ffc7e398f7dbd2d3d46e98013b1d3133bc7
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-17 18:01:38 +0800

    Create 64. 最小路径和.cpp

---
+++ 动态规划/线性DP/64. 最小路径和.cpp
@@ -0,0 +1,20 @@
+/*
+状态定义：
+dp[i][j]：走到第i行第j列格子的最小路径和
+状态转移：
+dp[i][j]+=min(dp[i-1][j],dp[i][j-1]);
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        for(int i=1;i<n;i++) grid[i][0]+=grid[i-1][0];
+        for(int i=1;i<m;i++) grid[0][i]+=grid[0][i-1];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[n-1][m-1];
+    }
+};
