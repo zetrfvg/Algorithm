1ec30bd996c100870089642cb475e848c4f8c412
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-18 11:06:55 +0800

    Update 300. 最长递增子序列.cpp

--- 动态规划/LIS/300. 最长递增子序列.cpp
+++ 动态规划/LIS/300. 最长递增子序列.cpp
@@ -18,3 +18,26 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+
+/*
+贪心+二分
+维护一个递增的序列
+枚举nums中的元素，在序列中二分查找找到第一个大于等于它的位置
+用nums[i]去更新这个序列中这个位置的值
+*/
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> g;
+        for(int i=0;i<n;i++){
+            if(!g.size()||nums[i]>g.back()){
+                g.push_back(nums[i]);
+            }else{
+                auto p=lower_bound(g.begin(),g.end(),nums[i])-g.begin();
+                g[p]=nums[i];               
+            }
+        }
+        return g.size();
+    }
+};
