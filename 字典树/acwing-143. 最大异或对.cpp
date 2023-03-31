c3414774f614b5b7792f80f00089508664bbb12b
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-03-31 11:01:40 +0800

    Create acwing-143. 最大异或对.cpp

---
+++ 字典树/acwing-143. 最大异或对.cpp
@@ -0,0 +1,48 @@
+/*
+01-Trie模板题
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int N=100010;
+int tr[31*N][2],cnt[31*N],idx;
+int a[N];
+
+void insert(int u){
+	int p=0;
+	for(int i=30;~i;i--){
+		int j=(u>>i)&1;
+		if(!tr[p][j]) tr[p][j]=++idx;
+		p=tr[p][j];
+	}
+}
+
+int query(int u){
+	int p=0,res=0;
+	for(int i=30;~i;i--){
+		int j=(u>>i)&1;
+		if(tr[p][!j]){
+			p=tr[p][!j];
+			res|=(1<<i);
+		}
+		else p=tr[p][j];
+	}
+	return res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int n;
+	cin>>n;
+	int ans=0;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		insert(a[i]);
+	}
+	for(int i=1;i<=n;i++){
+		ans=max(ans,query(a[i]));
+	}
+	cout<<ans;
+  	return 0;
+}
