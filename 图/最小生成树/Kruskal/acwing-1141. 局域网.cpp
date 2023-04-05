a045ae8a6aed3d59ff3bde6c8fa615091c72cb2b
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-04-05 21:33:01 +0800

    Create acwing-1141. 局域网.cpp

---
+++ 图/最小生成树/Kruskal/acwing-1141. 局域网.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+template<typename T>
+struct DSU{
+	int n;
+	vector<T> p,siz;
+	DSU(int _n):p(_n+1),siz(_n+1),n(_n){
+		iota(p.begin(),p.end(),0);
+		for(int i=0;i<=n;i++) siz[i]=1;
+	}
+	T findd(T x){
+		return p[x]==x?x:p[x]=findd(p[x]);
+	}
+	void unionn(T x,T y){
+		x=findd(x),y=findd(y);
+		if(x==y) return;
+		if(siz[x]>siz[y]) swap(x,y);
+		p[x]=y;
+		siz[y]+=siz[x];
+	}
+};
+
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int n,m;
+	cin>>n>>m;
+	DSU<int> dsu(n);
+	vector<vector<int>> e(m);
+	int ans=0;
+	for(int i=0;i<m;i++){
+		int x,y,w;
+		cin>>x>>y>>w;
+		e[i]={x,y,w};
+		ans+=w;
+	}
+	sort(e.begin(),e.end(),[](const auto &a,const auto &b){
+		return a[2]<b[2];
+	});
+	for(int i=0;i<m;i++){
+		int x=e[i][0],y=e[i][1],w=e[i][2];
+		x=dsu.findd(x),y=dsu.findd(y);
+		if(x!=y){
+			ans-=w;
+			dsu.unionn(x,y);
+		}
+	}
+	cout<<ans;
+  	return 0;
+}
