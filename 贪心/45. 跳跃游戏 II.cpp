054eccbf6660c736c0da322ed78fe51a41c54174
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-04-10 09:33:19 +0800

    Create 45. 跳跃游戏 II.cpp

---
+++ 贪心/45. 跳跃游戏 II.cpp
@@ -0,0 +1,21 @@
+/*
+维护一个最远可以抵达的距离，维护一个走k步最远能够到达的点
+贪心的更新即可
+*/
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0,mxpos=0,end=0;
+        for(int i=0;i<n-1;i++){
+            if(mxpos>=i){
+                mxpos=max(mxpos,i+nums[i]);
+                if(i==end){
+                    end=mxpos;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
