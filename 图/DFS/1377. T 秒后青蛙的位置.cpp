ee6cc0ca357666a0dc6e7f105c5c6f59937cc6ca
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-05-24 11:02:48 +0800

    Create 1377. T 秒后青蛙的位置.cpp

---
+++ 图/DFS/1377. T 秒后青蛙的位置.cpp
@@ -0,0 +1,35 @@
+/*
+①深搜的深度不超过target
+②能向下搜且有儿子的节点pro=0
+③叶子节点返回
+*/
+class Solution {
+public:
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        vector<vector<int>> e(n);
+        for(auto &p:edges){
+            int x=p[0]-1,y=p[1]-1;
+            e[x].push_back(y);
+            e[y].push_back(x);
+        }
+        vector<double> pro(n);
+        function<void(int,int,int,double)> dfs=[&](int x,int fa,int dep,double p){
+            pro[x]=p;
+            if(dep==t) return;
+            double cnt=0;
+            for(auto y:e[x]){
+                if(y!=fa) cnt+=1;
+            }
+            if(cnt>0){
+                pro[x]=0;
+                for(auto y:e[x]){
+                    if(y!=fa){
+                        dfs(y,x,dep+1,p/cnt);
+                    }
+                }
+            }
+        };
+        dfs(0,-1,0,1);
+        return pro[target-1];
+    }
+};
