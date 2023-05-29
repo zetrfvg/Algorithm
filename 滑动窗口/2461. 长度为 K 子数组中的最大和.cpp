b56923371690922e42d58ba2756273527685d876
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-05-29 16:44:57 +0800

    Create 2461. 长度为 K 子数组中的最大和.cpp

---
+++ 滑动窗口/2461. 长度为 K 子数组中的最大和.cpp
@@ -0,0 +1,21 @@
+/*
+滑动窗口+哈希表模拟即可
+*/
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> v(100010);
+        long long s=0,cnt=0,ans=0;
+        for(int l=0,r=0;r<n;r++){
+            s+=nums[r];
+            if(!v[nums[r]]++) cnt++;
+            if(cnt==k) ans=max(ans,s);
+            if(r>=k-1){
+                if(!--v[nums[l]]) cnt--;
+                s-=nums[l++];
+            }
+        }
+        return ans;
+    }
+};
