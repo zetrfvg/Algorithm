e0055417b435148852923ae2ae08dbac7b39a59b
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-05-30 16:34:57 +0800

    Create 76. 最小覆盖子串.cpp

---
+++ 双指针/76. 最小覆盖子串.cpp
@@ -0,0 +1,30 @@
+/*
+经典的双指针
+枚举所有元素作为右端点的最小合法窗口
+*/
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        vector<int> sc(128),tc(128);
+        for(auto c:t) tc[c]++;
+        auto check=[&](){
+            for(int i=0;i<128;i++){
+                if(sc[i]<tc[i]) return false;
+            }
+            return true;
+        };
+        int mn=1e9,pos;
+        for(int l=0,r=0;r<n;r++){
+            sc[s[r]]++;
+            while(check()){
+                if(r-l+1<mn){
+                    mn=r-l+1;
+                    pos=l;
+                }
+                sc[s[l++]]--;
+            }
+        }
+        return mn==1e9?"":s.substr(pos,mn);
+    }
+};
