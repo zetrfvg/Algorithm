4711e0aeda8ac10cfac7ea14f366327772b0258c
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-06-01 21:47:34 +0800

    Create 1876. 长度为三且各字符不同的子字符串.cpp

---
+++ 杂题/力扣/1876. 长度为三且各字符不同的子字符串.cpp
@@ -0,0 +1,13 @@
+/*
+模拟即可
+*/
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int ans=0;
+        for(int i=2;i<s.size();i++){
+            if(s[i]!=s[i-1]&&s[i]!=s[i-2]&&s[i-1]!=s[i-2]) ans++;
+        }
+        return ans;
+    }
+};
