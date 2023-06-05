b9e3787800c111763aa9ee843b6bad11be105219
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-06-05 16:25:53 +0800

    Create 2696. 删除子串后的字符串最小长度.cpp

---
+++ 栈/2696. 删除子串后的字符串最小长度.cpp
@@ -0,0 +1,15 @@
+/*
+栈模拟即可
+*/
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(auto c:s){
+            if(c=='B'&&!st.empty()&&st.top()=='A') st.pop();
+            else if(c=='D'&&!st.empty()&&st.top()=='C') st.pop();
+            else st.push(c);
+        }
+        return st.size();
+    }
+};
