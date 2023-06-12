dc38f488728e494cd85f457128bac22d7e3cecdb
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-06-12 19:07:57 +0800

    Create 1785. 构成特定和需要添加的最少元素.cpp

---
+++ 贪心/1785. 构成特定和需要添加的最少元素.cpp
@@ -0,0 +1,12 @@
+/*
+计算nums的和sum，求sum与目标值goal之差的绝对值
+每次贪心的选x，使abs(x)=limit，那么总共要选(delta+limit-1)/limit个
+*/
+class Solution {
+public:
+    int minElements(vector<int>& nums, int limit, int goal) {
+        long long sum=accumulate(nums.begin(),nums.end(),0LL);
+        long long delta=abs(sum-goal);
+        return (delta+limit-1)/limit;
+    }
+};
