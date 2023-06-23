967f953c0354c18bafb9abb318f9216689ea4b25
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-06-23 10:55:50 +0800

    Create 1492. n 的第 k 个因子.cpp

---
+++ 杂题/力扣/1492. n 的第 k 个因子.cpp
@@ -0,0 +1,15 @@
+/*
+试除法
+*/
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int t=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0&&(++t)==k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
