e85aa19e2b4911852863635c4c1ac1e3a83f99dc
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-07-03 12:01:54 +0800

    Create 445. 两数相加 II.cpp

---
+++ 栈/445. 两数相加 II.cpp
@@ -0,0 +1,32 @@
+/*
+栈模拟，头插法创建链表
+*/
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1,s2;
+        for(auto p=l1;p;p=p->next){
+            s1.push(p->val);
+        }
+        for(auto p=l2;p;p=p->next){
+            s2.push(p->val);
+        }
+        ListNode *ans=new ListNode(0),*p;
+        int rest=0;
+        while(!s1.empty()||!s2.empty()||rest){
+            if(!s1.empty()){
+                rest+=s1.top();
+                s1.pop();
+            }
+            if(!s2.empty()){
+                rest+=s2.top();
+                s2.pop();
+            }
+            p=new ListNode(rest%10);
+            p->next=ans->next;
+            ans->next=p;
+            rest/=10;
+        }
+        return ans->next;
+    }
+};
