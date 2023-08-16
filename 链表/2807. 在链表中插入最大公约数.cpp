59e1dc9ea9ff88a6f8316d09973862ae4c5b0a33
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-08-16 09:59:16 +0800

    Create 2807. 在链表中插入最大公约数.cpp

---
+++ 链表/2807. 在链表中插入最大公约数.cpp
@@ -0,0 +1,15 @@
+/*
+模拟链表插入过程即可
+*/
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode *l=head,*r=head->next;
+        while(r){
+            l->next=new ListNode(__gcd(l->val,r->val));
+            l->next->next=r;
+            l=r,r=r->next;
+        }
+        return head;
+    }
+};
