3c77d6cc7693aa4048e2a9f43ec7946be9345790
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-08-16 09:56:57 +0800

    Update 2682. 找出转圈游戏输家.cpp

--- 杂题/力扣/2682. 找出转圈游戏输家.cpp
+++ 杂题/力扣/2682. 找出转圈游戏输家.cpp
@@ -4,21 +4,14 @@
 class Solution {
 public:
     vector<int> circularGameLosers(int n, int k) {
-        vector<int> d(n);
-        vector<int> ans;
-        int v=1,p=0;
-        d[0]=1;
-        while(1){
-            int t=(p+v++*k)%n;
-            if(d[t]==1) break;
-            d[t]=1;
-            p=t;
+        vector<int> ans,vis(n);
+        vis[0]=1;
+        for(int i=1,p=0;;i++){
+            p=(p+k*i)%n;
+            if(vis[p]) break;
+            vis[p]++;
         }
-        for(int i=0;i<n;i++){
-            if(!d[i]){
-                ans.push_back(i+1);
-            }
-        }
+        for(int i=0;i<n;i++) if(!vis[i]) ans.push_back(i+1);
         return ans;
     }
 };
