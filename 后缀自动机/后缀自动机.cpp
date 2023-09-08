2cbab4673cca334c7dd97ab1d6cd11fb242449c8
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-09-08 11:26:32 +0800

    Update 后缀自动机.cpp

--- 后缀自动机/后缀自动机.cpp
+++ 后缀自动机/后缀自动机.cpp
@@ -2,20 +2,25 @@
 ch[x][c]：转移边，x通过字符c转移到的状态
 fa[x]：链接边，后缀链接，串str是节点x能表示的最短的子串，fa[x]则指向str的最长后缀的代表的状态
 len[x]：节点x能够表示的最长子串的长度
+cnt[x]：当前节点中的子串出现的次数
+dp[x]：当前节点向后走还能走出多少子串
 
 --整个就是不理解--
+性质：
+节点子串数量：len[x]-len[fa[x]]
+
 */
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-const int N=2e6+10;
-vector<int> e[N];
-ll cnt[N],ans;
+const int N=2e5+10;
+vector<int> e[N],dag[N];
+ll cnt[N],dp[N];
 int tot=1,np=1;
-int fa[N],ch[N][26],len[N];
+int fa[N],ch[N][26],len[N],deg[N];
 
-void extend(int c){
+void extend(int c){//建立SAM
 	int p=np;
 	np=++tot,len[np]=len[p]+1,cnt[np]=1;
 	for(;p&&!ch[p][c];p=fa[p]) ch[p][c]=np;
@@ -33,12 +38,50 @@ void extend(int c){
 	}
 }
 
-void dfs(int x){
+void dfs(int x){//求节点中子串出现的个数
 	for(auto y:e[x]){
 		dfs(y);
 		cnt[x]+=cnt[y];
 	}
-	if(cnt[x]>1) ans=max(ans,cnt[x]*len[x]);
+}
+
+void toposort(){//反向建图跑拓扑
+	for(int i=1;i<=tot;i++){
+		for(int j=0;j<26;j++){
+			if(!ch[i][j]) continue;
+			dag[ch[i][j]].push_back(i);
+			deg[i]++;
+		}
+	}
+	queue<int> q;
+	for(int i=1;i<=tot;i++) if(!deg[i]) q.push(i);
+	while(!q.empty()){
+		int x=q.front();
+		q.pop();
+		dp[x]++;
+		for(auto y:dag[x]){
+			dp[y]+=dp[x];
+			if(!--deg[y]) q.push(y);
+		}
+	}
+}
+
+string kth(int k){//求字典序第k小的子串
+	string res;
+	int p=1;
+	while(k){
+		for(int i=0;i<26;i++){
+			if(!ch[p][i]) continue;
+			if(dp[ch[p][i]]<k) k-=dp[ch[p][i]];
+			else{
+				res+='a'+i;
+				p=ch[p][i];
+				k--;
+				break;
+			}
+		}
+	}
+	return res;
 }
 
 int main(){
@@ -46,8 +89,14 @@ int main(){
 	string s;
 	cin>>s;
 	for(auto c:s) extend(c-'a');
-	for(int i=2;i<=tot;i++) e[fa[i]].push_back(i);
-	dfs(1);
-	cout<<ans;
+	toposort();
+	int q;
+	cin>>q;
+	while(q--){
+		int k;
+		cin>>k;
+		cout<<kth(k)<<'\n';
+	}
 	return 0;
 }
+
