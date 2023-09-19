1a0452f51233adb39d67e3d6ae637dbd73ee36f1
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-09-19 16:05:19 +0800

    Update 2860. 让所有学生保持开心的分组方法数.cpp

--- 杂题/力扣/2860. 让所有学生保持开心的分组方法数.cpp
+++ 杂题/力扣/2860. 让所有学生保持开心的分组方法数.cpp
@@ -3,13 +3,13 @@
 */
 class Solution {
 public:
-    int countWays(vector<int>& a) {
-        sort(a.begin(),a.end());
-        int ans=a[0]>0;
-        int n=a.size();
-        for(int i=0;i<a.size();i++){
-            if(a[i]<i+1){
-                if(i!=n-1&&a[i+1]>i+1) ans++;
+    int countWays(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=nums[0]>0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<i+1){
+                if(i!=n-1&&nums[i+1]>i+1) ans++;
                 else if(i==n-1) ans++;
             }
         }
