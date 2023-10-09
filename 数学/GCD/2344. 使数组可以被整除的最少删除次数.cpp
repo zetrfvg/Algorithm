2556e06c7013f809c7c931137b7401ec48756bab
zetrfvg <120292018+zetrfvg@users.noreply.github.com>
2023-10-09 15:10:17 +0800

    Update 2344. 使数组可以被整除的最少删除次数.cpp

--- 数学/GCD/2344. 使数组可以被整除的最少删除次数.cpp
+++ 数学/GCD/2344. 使数组可以被整除的最少删除次数.cpp
@@ -3,12 +3,9 @@
 */
 class Solution {
 public:
-    int gcd(int a,int b){
-        return b?gcd(b,a%b):a;
-    }
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        long long g=0;
-        for(auto d:numsDivide) g=gcd(g,d);
+        int g=0;
+        for(auto d:numsDivide) g=__gcd(g,d);
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++) if(g%nums[i]==0) return i;
         return -1;
